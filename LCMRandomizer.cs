6416b62bd850de304062fc3bfb8326d247116da9
AriPoetra <100426610+AriPoetra@users.noreply.github.com>
2024-10-01 13:27:26 +0700

    Add files via upload

---
+++ LCMRandomizer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace TA
+{
+    public class LCMRandomizer
+    {
+        private int a; // multiplier
+        private int c; // increment
+        private int m; // modulus
+        private int seed; // initial seed value
+        private int current; // current value in the sequence
+
+        public LCMRandomizer(int a, int c, int m, int seed)
+        {
+            this.a = a;
+            this.c = c;
+            this.m = m;
+            this.seed = seed;
+            this.current = seed;
+        }
+
+        public int Next()
+        {
+            current = (a * current + c) % m;
+            return current;
+        }
+    }
+}
---
+++ QuestionManager.cs
@@ -0,0 +1,25 @@
+﻿namespace TA.Properties
+{
+    public class QuestionManager : MonoBehaviour
+    {
+        // LCM Randomizer
+        private LCMRandomizer randomizer;
+
+        // Question database
+        public QuestionDatabase questionDatabase;
+
+        // Current question index
+        private int currentQuestionIndex;
+
+        void Start()
+        {
+            // Create an instance of the LCMRandomizer class
+            randomizer = new LCMRandomizer(1664525, 1013904223, 4294967296, 42);
+
+            // Initialize the question database
+            questionDatabase = Resources.Load<QuestionDatabase>("QuestionDatabase");
+
+            // Set the current question index to 0
+            currentQuestionIndex = 0;
+        }
+}
